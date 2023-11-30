55dec8dfbb0cd9b6b99902c6894eb30162f15fec
prajakta2055 <114821881+prajakta2055@users.noreply.github.com>
2023-11-30 09:39:11 -0600

    Add files via upload
    Mysql script

---
+++ Mysql.sql
@@ -0,0 +1,47 @@
+create database Logistic;
+
+use Logistic;
+
+create table user (username varchar(255), password varchar(255), usertype varchar(255));
+
+INSERT INTO user (username, password, usertype) VALUES
+  ('user1', 'pass123', 'admin'),
+  ('user2', 'pass456', 'customer'),
+  ('user3', 'pass789', 'customer'),
+  ('user4', 'passabc', 'admin'),
+  ('user5', 'passdef', 'customer');
+  
+
+
+
+select * from user;
+select * from orders;
+
+
+CREATE TABLE orders (
+    orderId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    customer VARCHAR(255),
+    itemWeight VARCHAR(255),
+    packageDimensions VARCHAR(255),
+    carrierName VARCHAR(255),
+    dateOrdered VARCHAR(255),
+    destination VARCHAR(255),
+    logo VARCHAR(255),
+    price VARCHAR(255),
+    phone_number VARCHAR(15),
+    email_address VARCHAR(255),
+    address VARCHAR(255),
+    payment_method VARCHAR(255),
+    card_no VARCHAR(255),
+    zipcode VARCHAR(255),
+    fromLocation VARCHAR(255)
+);
+
+
+select * from orders;
+
+
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'Oneplus@6';
+
+Flush privileges;
